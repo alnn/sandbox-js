604f268febd8958e50ea3690501141eade0be9f7
ernium <ernium@gmail.com>
2016-08-10 09:45:27 +0300

    new patterns learned

--- nodejs-test/patterns.js
+++ nodejs-test/patterns.js
@@ -245,3 +245,110 @@ saleLst.decorate('fedtax');
 saleLst.decorate('quebec');
 saleLst.decorate('money');
 console.log(saleLst.getPrice());
+
+console.log('********************************************');
+
+//STRATEGY
+var validator = {
+    types: {},
+    messages: [],
+    config: {},
+    validate: function(data) {
+        var i, msg, type, checker, result_ok;
+
+        this.messages = [];
+
+        for (i in data) {
+            if (!data.hasOwnProperty(i)) {
+                continue;
+            }
+            type = this.config[i];
+            checker = this.types[type];
+
+            if (!type) {
+                continue;
+            }
+
+            if (!checker) {
+                throw {
+                    name: "ValidationError",
+                    message: "No handler to validate type " + type
+                };
+            }
+
+            result_ok = checker.validate(data[i]);
+            if (!result_ok) {
+                msg = "Invalid value for *" + i + "*, " + checker.instructions;
+                this.messages.push(msg);
+            }
+        }
+        return this.hasErrors();
+    },
+    hasErrors: function() {
+        return this.messages.length !== 0;
+    }
+};
+
+validator.types.isNonEmpty = {
+    validate: function(value) {
+        return value !== "";
+    },
+    instructions: "the value cannot be empty"
+};
+
+validator.types.isNumber = {
+    validate: function(value) {
+        return !isNaN(value);
+    },
+    instructions: "the value can only be a valid number, e.g. 1, 3.14 or 222"
+};
+
+validator.types.isAlphaNum = {
+    validate: function(value) {
+        return !/[^a-z0-9]/i.test(value);
+    },
+    instructions: "the value can only contain characters and numbers, no special symbols"
+};
+
+validator.config = {
+    first_name: 'isNonEmpty',
+    age: 'isNumber',
+    username: 'isAlphaNum'
+};
+
+var data = {
+    first_name: "Super",
+    last_name: "Man",
+    age: "unknown",
+    username: "o_O'"
+};
+
+validator.validate(data);
+if (validator.hasErrors()) {
+    console.log(validator.messages.join("\n"));
+}
+
+console.log('********************************************');
+
+//FACADE
+var myEvent = {
+    // ...
+    stop: function(e) {
+        // IE
+        if (typeof e.returnValue === "boolean") {
+            e.returnValue = false;
+        }
+        if (typeof e.cancelBubble === "boolean") {
+            e.cancelBubble = true;
+        }
+        // Others
+        if (typeof e.preventDefault === "function") {
+            e.preventDefault();
+        }
+        if (typeof e.stopPropagation === "function") {
+            e.stopPropagation();
+        }
+    }
+};
+
+

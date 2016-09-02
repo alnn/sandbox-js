35f6925f0390321404c1c0d275adf388bd74fa80
ernium <ernium@gmail.com>
2016-09-02 20:38:31 +0300

    about set routerWillLeave hook

--- react-tutorials/getting-started-with-react-router/react-router-setup/src/App.js
+++ react-tutorials/getting-started-with-react-router/react-router-setup/src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import {Router, Route, Link, Redirect, hashHistory} from 'react-router';
+import {Router, Route, Link, hashHistory} from 'react-router';
 
-const Home = () => <div><h1>Home</h1><Links /></div>;
-const About = () => <div><h1>About Us</h1><Links /></div>;
-const Contact = () => <div><h1>Contact</h1><Links /></div>;
+class Home extends React.Component {
+    componentWillMount() {
+        this.context.router.setRouteLeaveHook(
+            this.props.route,
+            this.routerWillLeave
+        );
+    }
+    routerWillLeave(nextLocation) {
+        //console.log(`leaving home for ${nextLocation.pathname}`);
+        return `leaving home for ${nextLocation.pathname}`;
+    }
+    render() {
+        return <div><h1>Home</h1><Links /></div>;
+    }
+}
 
+Home.contextTypes = {
+    router: React.PropTypes.object.isRequired
+};
+
+const About = () => <div><h1>About</h1><Links /></div>;
 const Links = () =>
     <nav>
         <Link to="/">Home</Link>
-        <Link to="about-us">About Us</Link>
         <Link to="about">About</Link>
-        <Link to="contact">Contact</Link>
     </nav>
 
 const App = () => {
     return (
         <Router history={hashHistory}>
             <Route path="/" component={Home}></Route>
-            <Route path="/about-us" component={About}></Route>
-            <Route path="/contact" component={Contact}></Route>
-            <Redirect from="/about" to="/about-us"></Redirect>
+            <Route path="/about" component={About}></Route>
         </Router>
     );
 }
